d94edd0cf6156b458ea50182d1149816ecd82674
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2024-03-10 16:47:08 +0100

    preventing unauthorized users to access chat path

--- middleware.ts
+++ middleware.ts
@@ -7,17 +7,17 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = req.nextUrl;
 
-  if (token && pathname.startsWith("/auth/login")) {
-    return NextResponse.redirect(new URL("/", req.url));
+  if (!token && (pathname.startsWith("/chat") || pathname === "/register")) {
+    return NextResponse.redirect(new URL("/auth/login", req.url));
   }
 
-  if (!token && pathname.startsWith("/register")) {
-    return NextResponse.redirect(new URL("/auth/login", req.url));
+  if (token && pathname === "/auth/login") {
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/auth/login", "/register"],
+  matcher: ["/auth/login", "/register", "/chat/:path*"],
 };
